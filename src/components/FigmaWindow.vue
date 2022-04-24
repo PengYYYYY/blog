e0db7ebd2138406384994469f7ecded0895ec150
ivringpeng <ivringpeng@tencent.com>
2022-04-25 00:07:23 +0800

    feat: add figmawindow

---
+++ src/components/FigmaWindow.vue
@@ -0,0 +1,23 @@
+<template>
+  <iframe class="figmaFile" :src="fileSrc" allowfullscreen></iframe>
+</template>
+<script setup lang="ts">
+import { defineProps, computed } from 'vue'
+const props = defineProps({
+  url: String,
+})
+
+const fileSrc = computed(() => {
+  return `https://www.figma.com/embed?embed_host=share&url=${encodeURIComponent(
+    props.url
+  )}`
+})
+</script>
+
+<style scoped>
+.figmaFile {
+  width: 100%;
+  height: 400px;
+  border: 1px solid rgba(0, 0, 0, 0.1);
+}
+</style>
--- src/graphics/index.md
+++ src/graphics/index.md
@@ -1,1 +1,7 @@
 # 图形学
+
+<script setup>
+import FigmaWindow from '../components/FigmaWindow.vue'
+</script>
+
+<FigmaWindow url="https://www.figma.com/file/MqInZ0fPb0paFi2WgG7BVN/study-map?node-id=0%3A1" page="test" />
